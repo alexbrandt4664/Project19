24a7f62b9ac05618092b25b18b8e157dc521b29b
Alex Brandt <brandta7@msu.edu>
2020-10-07 01:36:49 -0400

    Created game activity and pass data to it

---
+++ app/src/main/java/edu/msu/brandta7/project1_9/GameActivity.java
@@ -0,0 +1,14 @@
+package edu.msu.brandta7.project1_9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/edu/msu/brandta7/project1_9/MainActivity.java
+++ app/src/main/java/edu/msu/brandta7/project1_9/MainActivity.java
@@ -2,7 +2,11 @@ package edu.msu.brandta7.project1_9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createGame(View view) {
+
+        // Read the contents of the user input
+        String aName = ((EditText)findViewById(R.id.aName)).getText().toString();
+        String bName = ((EditText)findViewById(R.id.bName)).getText().toString();
+
+        // Check if either of the inputs were empty
+        if (aName.equals("") || bName.equals("")) {
+            Toast.makeText(getApplicationContext(), getString(R.string.emptyInput), Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent intent = new Intent(this, GameActivity.class);
+            intent.putExtra(getString(R.string.aKey), aName);
+            intent.putExtra(getString(R.string.bKey), bName);
+            startActivity(intent);
+        }
     }
 }
